4d1347a21ceb4b92fb71791fdd8ecd2e705a4875
Anderson <tandersonf@hotmail.com>
2023-04-09 09:29:22 -0300

    initial commit

---
+++ tip-calculator/tip-calculator/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  tip-calculator
+//
+//  Created by Anderson Oliveira on 09/04/23.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Loaded")
+    }
+}
